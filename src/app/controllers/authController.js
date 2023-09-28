abbc4d8e52263dad493b75fe52da8ac93044c879
NTT1410 <nguyentantru14102002@gmail.com>
2023-09-29 04:38:18 +0700

    push

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -44,7 +44,7 @@ module.exports.signup_get = (req, res) => {
 	res.render("signup", { layout: false });
 };
 module.exports.login_get = (req, res) => {
-	res.render("login", { layout: false });
+	res.render("login");
 };
 module.exports.signup_post = async (req, res) => {
 	const day_of_birth = moment(req.body.day_of_birth, "DD-MM-YYYY");
